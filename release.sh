c82afe14b15e4428911771ab58d8ac51adb1021a
Sebastien Besson <seb.besson@gmail.com>
2020-10-12 11:18:03 +0100

    Fix archives and ignore zip

--- release.sh
+++ release.sh
@@ -1,14 +1,16 @@
 #! /usr/bin/env bash
 # Create archives of submission templates for HCS/non-HCS
 
-mkdir hcs_template
-cp idr0000-study_HCS.txt hcs_template/idr0000-study.txt
-mkdir hcs_template/screenA
-cp screenA/*.txt hcs_template/screenA
-zip idr_hcs_submission_template.zip idr_hcs_template/
+mkdir idr_hcs_template
+cp idr0000-study_HCS.txt idr_hcs_template/idr0000-study.txt
+mkdir idr_hcs_template/screenA
+cp screenA/*.txt idr_hcs_template/screenA
+zip -r idr_hcs_submission_template.zip idr_hcs_template/
+rm -rf idr_hcs_template
 
-mkdir experiment_template
-cp idr0000-study_nonHCS.txt experiment_template/idr0000-study.txt
-mkdir experiment_template/experimentB
-cp experimentB/*.txt experiment_template/experimentB
-zip idr_experiment_submission_template.zip idr_experiment_template/
+mkdir idr_experiment_template
+cp idr0000-study_nonHCS.txt idr_experiment_template/idr0000-study.txt
+mkdir idr_experiment_template/experimentB
+cp experimentB/*.txt idr_experiment_template/experimentB
+zip -r idr_experiment_submission_template.zip idr_experiment_template/
+rm -rf idr_experiment_template
